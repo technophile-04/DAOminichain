3a915a5197fccc824f62bce7c6fa3ac413b20a72
Shiv Bhonde <shivbhonde04@gmail.com>
2023-07-22 22:40:53 +0200

    add mint token functionality

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -2,7 +2,8 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
+import { useIsMounted } from "usehooks-ts";
+import { useAccount, useBalance, useNetwork, useSwitchNetwork } from "wagmi";
 import { ArrowsRightLeftIcon, Bars3Icon, BugAntIcon, ChevronDownIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { QueueListIcon } from "@heroicons/react/24/solid";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
@@ -35,9 +36,18 @@ export const Header = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const burgerMenuRef = useRef<HTMLDivElement>(null);
   const { setUiChain, uiChain } = useGlobalState();
-  const { isConnected } = useAccount();
+  const { isConnected, address: connectedAddress } = useAccount();
   const { chain } = useNetwork();
   const { switchNetwork } = useSwitchNetwork();
+  const isMounted = useIsMounted();
+
+  const { data: footyTokenBalance } = useBalance({
+    address: connectedAddress,
+    token: "0xAb1cE3C12a85B7FA613DE482bfD3a731E7B8C28e",
+    watch: true,
+  });
+
+  console.log("FootyTokenBalance", footyTokenBalance);
 
   useEffect(() => {
     (async () => {
@@ -55,9 +65,6 @@ export const Header = () => {
 
   const navLinks = (
     <>
-      <li>
-        <NavLink href="/">Home</NavLink>
-      </li>
       <li>
         <NavLink href="/debug">
           <BugAntIcon className="h-4 w-4" />
@@ -86,73 +93,81 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
-      <div className="navbar-start w-auto lg:w-1/2">
-        <div className="lg:hidden dropdown" ref={burgerMenuRef}>
-          <label
-            tabIndex={0}
-            className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
-            onClick={() => {
-              setIsDrawerOpen(prevIsOpenState => !prevIsOpenState);
-            }}
-          >
-            <Bars3Icon className="h-1/2" />
-          </label>
-          {isDrawerOpen && (
-            <ul
+    isMounted() && (
+      <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
+        <div className="navbar-start w-auto lg:w-1/2">
+          <div className="lg:hidden dropdown" ref={burgerMenuRef}>
+            <label
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
               onClick={() => {
-                setIsDrawerOpen(false);
+                setIsDrawerOpen(prevIsOpenState => !prevIsOpenState);
               }}
             >
-              {navLinks}
+              <Bars3Icon className="h-1/2" />
+            </label>
+            {isDrawerOpen && (
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                onClick={() => {
+                  setIsDrawerOpen(false);
+                }}
+              >
+                {navLinks}
+              </ul>
+            )}
+          </div>
+          <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6">
+            <div className="flex relative w-10 h-10">
+              <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            </div>
+            <div className="flex flex-col">
+              <span className="font-bold leading-tight">Scaffold-eth</span>
+              <span className="text-xs">Ethereum dev stack</span>
+            </div>
+          </Link>
+          <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
+        </div>
+        <div className="navbar-end flex-grow mr-4 space-x-4">
+          {/*Network switch button*/}
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-success btn-sm dropdown-toggle">
+              <span>{uiChain.name}</span>
+              <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
+            </label>
+            <ul tabIndex={0} className="dropdown-content menu p-2 mt-1 shadow-lg bg-base-100 rounded-box">
+              {enabledChains.map(chain => (
+                <li key={chain.id}>
+                  <button
+                    className="menu-item"
+                    type="button"
+                    onClick={() => {
+                      setUiChain(chain);
+                      if (isConnected) {
+                        switchNetwork?.(chain.id);
+                      }
+                    }}
+                  >
+                    <ArrowsRightLeftIcon className="h-6 w-4 ml-2 sm:ml-0" />
+                    <span className="whitespace-nowrap">
+                      Switch to <span style={{ color: NETWORKS_EXTRA_DATA[chain.id].color }}>{chain.name}</span>
+                    </span>
+                  </button>
+                </li>
+              ))}
             </ul>
-          )}
-        </div>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6">
-          <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
-          <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-eth</span>
-            <span className="text-xs">Ethereum dev stack</span>
+          <RainbowKitCustomConnectButton />
+          <FaucetButton />
+          <div className="flex flex-col items-center justify-center">
+            <>
+              <span>{footyTokenBalance?.formatted}</span>
+              <span className="text-[0.8em] font-bold ml-1 text-green-600">{footyTokenBalance?.symbol}</span>
+            </>
           </div>
-        </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
-      </div>
-      <div className="navbar-end flex-grow mr-4 space-x-4">
-        {/*Network switch button*/}
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-success btn-sm dropdown-toggle">
-            <span>{uiChain.name}</span>
-            <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
-          </label>
-          <ul tabIndex={0} className="dropdown-content menu p-2 mt-1 shadow-lg bg-base-100 rounded-box">
-            {enabledChains.map(chain => (
-              <li key={chain.id}>
-                <button
-                  className="menu-item"
-                  type="button"
-                  onClick={() => {
-                    setUiChain(chain);
-                    if (isConnected) {
-                      switchNetwork?.(chain.id);
-                    }
-                  }}
-                >
-                  <ArrowsRightLeftIcon className="h-6 w-4 ml-2 sm:ml-0" />
-                  <span className="whitespace-nowrap">
-                    Switch to <span style={{ color: NETWORKS_EXTRA_DATA[chain.id].color }}>{chain.name}</span>
-                  </span>
-                </button>
-              </li>
-            ))}
-          </ul>
         </div>
-        <RainbowKitCustomConnectButton />
-        <FaucetButton />
       </div>
-    </div>
+    )
   );
 };
